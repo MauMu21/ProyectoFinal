691c62b4fed8aa539da72df623da880d8f585c3d
MauMu21 <79818178+MauMu21@users.noreply.github.com>
2021-03-04 16:04:15 -0400

    Add files via upload

---
+++ ProyectoFinal/proyectofinal/src/utils/ConfigGrades.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class ConfigGrades {
+    public static Double PARCIAL = 0.5;
+    public static Double FINAL = 0.3;
+    public static Double HOMEWORKS = 0.2;
+    public static Double MIN_GRADES = 51.0;
+}
